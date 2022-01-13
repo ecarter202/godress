3c71b2e6a59d4a2813a994edb1204b6c6c5baf4b
eythan <ecarter202@gmail.com>
2022-01-13 10:55:06 -0700

    updating

--- unit_terms.go
+++ unit_terms.go
@@ -23,7 +23,7 @@ func ScrubUnit(address string) string {
 	addressX := strings.Split(strings.TrimSpace(address), " ")
 	for i, w := range addressX {
 		if _, contains := unitTerms[strings.ToLower(w)]; contains {
-			return ScrubUnit(strings.Join(append(addressX[:i], addressX[i+1:]...), " "))
+			return ScrubUnit(strings.Join(append(addressX[:i]), " "))
 		}
 	}
 
